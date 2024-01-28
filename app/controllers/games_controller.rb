e670c4ff2ec95f194cd33eccc965da0bf636b04e
Yaroslav Kuzhakhmetov <93444486+klyakijibido@users.noreply.github.com>
2024-01-28 20:54:33 +0300

    Added GamesController

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,17 @@
+class GamesController < ApplicationController
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def index
+
+  end
+
+  def play
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :games, only: [:create, :show, :index] do
+    post :play, on: :member
+  end
 end
